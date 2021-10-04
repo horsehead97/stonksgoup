49565a576692d492b8db6df1c6980c1eb0fb6129
horsehead97 <horsehead97@gmail.com>
2021-10-04 18:01:10 -0400

    more javascript

---
+++ jsSkewl/slider/js/fadeslide.js
@@ -0,0 +1,24 @@
+(function(){
+    "use strict";
+    const myImages=['image1.jpg','image2.jpg','image3.jpg','image4.jpg','image5.jpg'];
+    let current_image=0;
+    const mycontainer=getElementById('content');
+    const next=document.getElementById('next');
+    const prev=document.getElementById('previous');
+    const myimage=document.getElementById('myimage');
+    const newSlide=document.createElement('img');
+    newSlide.src=`img/${myImages[current_image]}`;
+    newSlide.className="fadeinimg";
+    mycontainer.appendChild(newSlide);
+    next.addEventListener('click',function(event){
+        event.preventDefault
+        current_image++;
+        if(current_image>myImages.length-1){current_image=0;}
+        myimage.src='img/'+myImages[current_image];
+    }); 
+    prev.addEventListener('click',function(){
+        current_image--;
+        if(current_image<0){current_image=myImages.length-1;}
+        myimage.src='img/'+myImages[current_image];
+    });  
+})();
